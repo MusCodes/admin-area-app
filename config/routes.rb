fa560a1dce9045e28df0e790a2bbb53f2b1f4b08
Mustafa <MustafaGino1@yahoo.com>
2023-10-13 01:07:24 -0500

    added a way to edit posts, including action buttons.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
+
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show]
 
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  authenticate :user do
+    ActiveAdmin.routes(self)
+  end
+  # Defines the root path route ("/")
+  # root "articles#index"
 end

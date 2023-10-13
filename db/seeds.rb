b5e32433af380dc65bcbcb60526e037a59dacbe0
Mustafa <MustafaGino1@yahoo.com>
2023-10-13 02:19:10 -0500

    added comment to the seed.rb.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Post.create(id: 1, title: 'First Post', description: 'This is the first post.', created_at: Time.current, updated_at: Time.current)
-Post.create(id: 2, title: 'Second Post', description: 'This is the second post.', created_at: Time.current, updated_at: Time.current)
-Post.create(id: 3, title: 'Third Post', description: 'This is the third post.', created_at: Time.current, updated_at: Time.current)
+
+# Creates seeds for the post table
+
+# Post.create(id: 1, title: 'First Post', description: 'This is the first post.', created_at: Time.current, updated_at: Time.current)
+# Post.create(id: 2, title: 'Second Post', description: 'This is the second post.', created_at: Time.current, updated_at: Time.current)
+# Post.create(id: 3, title: 'Third Post', description: 'This is the third post.', created_at: Time.current, updated_at: Time.current)
